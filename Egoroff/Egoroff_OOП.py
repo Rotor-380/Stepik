02eb8befc7a2007648d2bb09f9a1b03d26675d44
Rostislav <rotor-380@mail.ru>
2022-03-25 23:10:36 +0300

    modify

--- Egoroff/Egoroff_OOП.py
+++ Egoroff/Egoroff_OOП.py
@@ -91,14 +91,14 @@ p1.get_distance('10')
 # kokorin.score()
 # kokorin.statistics() # выводит "Kokorin Alex - голы: 1, передачи: 0"
 
-class Zebra:
-    def __init__(self):
-        self.stripe = ["Полоска белая", "Полоска черная"]
-
-    def which_stripe(self):
-        print(self.stripe[0])
-        self.stripe[0], self.stripe[1] = self.stripe[1], self.stripe[0]
+# class Zebra:
+#     def __init__(self):
+#         self.stripe = ["Полоска белая", "Полоска черная"]
 #
+#     def which_stripe(self):
+#         print(self.stripe[0])
+#         self.stripe[0], self.stripe[1] = self.stripe[1], self.stripe[0]
+# #
 #
 # z1 = Zebra()
 # z1.which_stripe()  # печатает "Полоска белая"
@@ -139,45 +139,40 @@ class Zebra:
 # print(jack.description()) # распечатает 'Jack is 4 years old'
 # print(jack.speak("Woof Woof")) # распечатает 'Jack says Woof Woof'
 # print(jack.speak("Bow Wow")) # распечатает 'Jack says Bow Wow'
-# class Stack():
-#     def __init__(self):
-#         self.values = []
-#
-#     def push(self, item):
-#         self.items = item
-#         self.values.append(self.items)
-#
-#     def pop(self):
-#         return self.values.pop(-1) if len(self.values) != 0 else print("Empty Stack")
-#
-#     def peek(self):
-#         return self.values[-1] if len(self.values) != 0 else print("Empty Stack")
-#         # if len(self.values) != 0:
-#         #     return self.values[-1]
-#         # elif len(self.values) == 0:
-#         #     print("Empty Stack")
-#         #     return None
-#
-#     def is_empty(self):
-#         return len(self.values) == 0
-#
-#     def size(self):
-#         return len(self.values)
-#
-#
-# s = Stack()
-# s.peek()  # распечатает 'Empty Stack'
-# print(s.is_empty())  # распечатает True
-# s.push('cat')  # кладем элемент 'cat' на вершину стека
-# s.push('dog')  # кладем элемент 'dog' на вершину стека
-# print(s.peek())  # распечатает 'dog'
-# s.push(True)  # кладем элемент True на вершину стека
-# print(s.size())  # распечатает 3
-# print(s.is_empty())  # распечатает False
-# s.push(777)  # кладем элемент 777 на вершину стека
-# print(s.pop())  # удаляем элемент 777 с вершины стека и печатаем его
-# print(s.pop())  # удаляем элемент True с вершины стека и печатаем его
-# print(s.size())  # распечатает 2
+class Stack():
+    def __init__(self):
+        self.values = []
+
+    def push(self, item):
+        #self.items = item
+        self.values.append(item)
+
+    def pop(self):
+        return self.values.pop(-1) if len(self.values) != 0 else print("Empty Stack")
+
+    def peek(self):
+        return self.values[-1] if len(self.values) != 0 else print("Empty Stack")
+
+    def is_empty(self):
+        return len(self.values) == 0
+
+    def size(self):
+        return len(self.values)
+
+
+s = Stack()
+s.peek()  # распечатает 'Empty Stack'
+print(s.is_empty())  # распечатает True
+s.push('cat')  # кладем элемент 'cat' на вершину стека
+s.push('dog')  # кладем элемент 'dog' на вершину стека
+print(s.peek())  # распечатает 'dog'
+s.push(True)  # кладем элемент True на вершину стека
+print(s.size())  # распечатает 3
+print(s.is_empty())  # распечатает False
+s.push(777)  # кладем элемент 777 на вершину стека
+print(s.pop())  # удаляем элемент 777 с вершины стека и печатаем его
+print(s.pop())  # удаляем элемент True с вершины стека и печатаем его
+print(s.size())  # распечатает 2
 # class UserMail:
 #     def __init__(self, login, email):
 #         self.login = login
